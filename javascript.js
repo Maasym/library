1caa6490e046ca4f7f620446e70a8ee0183dc56f
maasym <github1@go-nihon.com>
2021-12-09 18:59:17 +0100

    local upload

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 let myLibrary = [];
 
+
 const openModalBtn = document.querySelector('#open-modal-btn');
 const closeModalBtn = document.querySelector('#close-modal-btn');
 const addBookBtn = document.querySelector('#add-book-btn');
